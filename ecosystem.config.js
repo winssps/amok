cd907872946529a8923e37d9cc4964e58d2ec338
winssps <365423254@qq.com>
2017-12-18 13:51:43 +0800

    update ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,7 @@ module.exports = {
     // First application
     {
       name      : 'kos-oss',
-  //    script    : '/server/client.js',
+      script    : '/server/client.js',
       env: {
         COMMON_VARIABLE: 'true'
       },
